eca5d9fa5522ce8b57b7d76ff941edaff4750ad0
umitsayin <umitsayin1661@gmail.com>
2022-03-11 21:45:42 +0300

    deneme

---
+++ src/main/java/com/springsecurity/userservice/controller/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.springsecurity.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/api/login")
+    public String login(){
+        return "dasdasd";
+    }
+}
---
+++ src/main/java/com/springsecurity/userservice/entities/Role.java
@@ -0,0 +1,21 @@
+package com.springsecurity.userservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+}
